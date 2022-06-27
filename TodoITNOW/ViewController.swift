be664c3d66718d9514195cbd954fa0969b302b9c
Dimabonder <dimabonder15@gmail.com>
2022-06-27 21:54:17 +0300

    Initial Commit

---
+++ TodoITNOW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoITNOW
+//
+//  Created by Dima Bonder on 27/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
